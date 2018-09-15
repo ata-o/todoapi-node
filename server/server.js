0c0fe5ad7003d2c6706173733fa6b9fa65336d1d
ozymandias <onuratasaritas@gmail.com>
2018-09-15 23:04:06 +0300

    Added /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,6 +32,23 @@ app.get('/todos', (req, res) => {
   });
 
 })
+
+// GET /todos/1234324
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if (ObjectID.isValid(id)) {
+      Todo.findById(id).then((todo) => {
+        if (todo) {
+          res.status(200).send({todo});
+        } else {
+          res.status(404).send();
+        }
+      }).catch((e) => res.status(400).send(e));
+    } else {
+      res.status(404).send();
+    }
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
